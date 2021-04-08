c0f55c77fd5dcf9a3a09e265c5818fdb1dbd11a4
louizimoncef <64980863+louizimoncef@users.noreply.github.com>
2021-04-08 23:37:59 +0100

    Add files via upload

---
+++ emsi-blockchain/crypto/sha256.c
@@ -0,0 +1,21 @@
+#include "hblk_crypto.h"
+
+uint8_t* sha256(int8_t const* s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+{
+    digest = NULL;
+    if (s) {
+        EVP_MD_CTX* mdctx;
+        const EVP_MD* md;
+        int md_len;
+        md = EVP_sha256();
+        mdctx = EVP_MD_CTX_create();
+        EVP_DigestInit_ex(mdctx, md, NULL);
+        EVP_DigestUpdate(mdctx, s, strlen(s));
+        EVP_DigestFinal_ex(mdctx, digest, &md_len);
+        EVP_MD_CTX_destroy(mdctx);
+        EVP_cleanup();
+    }
+  
+
+    return digest ? digest : NULL;
+}
